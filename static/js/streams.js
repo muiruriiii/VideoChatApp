7df48c52b18f8040b0e2f044c29bafc6dd529dae
muiruriiii <muiruricynthiaaa@gmail.com>
2022-10-09 20:41:10 +0300

    Initial commit Agora Setup

---
+++ static/js/streams.js
@@ -0,0 +1,5 @@
+const APP_ID = 'af723613451d48059ed4452bf1a0fcc5'
+const CHANNEL = 'main'
+const TOKEN = '007eJxTYDhV9vLKXR+51dGH6k40Oaw4Ypz2wD5Ju6SjsvvMXK3WeawKDIlp5kbGZobGJqaGKSYWBqaWqSkmJqZGSWmGiQZpycmmP9mckysiXJLvFK5jYmSAQBCfhSE3MTOPgQEAs80hhg=='
+
+console.log('Stream.js Connected')

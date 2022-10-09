a1eb687a1fa89b7bc013a21225d8aca77b329e06
Cynthia Muiruri <60257172+muiruriiii@users.noreply.github.com>
2022-10-09 20:48:18 +0300

    Update streams.js

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,5 +1,5 @@
-const APP_ID = 'af723613451d48059ed4452bf1a0fcc5'
-const CHANNEL = 'main'
-const TOKEN = '007eJxTYDhV9vLKXR+51dGH6k40Oaw4Ypz2wD5Ju6SjsvvMXK3WeawKDIlp5kbGZobGJqaGKSYWBqaWqSkmJqZGSWmGiQZpycmmP9mckysiXJLvFK5jYmSAQBCfhSE3MTOPgQEAs80hhg=='
+const APP_ID = ''
+const CHANNEL = ''
+const TOKEN = ''
 
 console.log('Stream.js Connected')
